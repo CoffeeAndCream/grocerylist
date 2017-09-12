5e08398476dc1f17b32a42eb6eced43464ac73e1
CoffeeAndCream <seanjhulse@gmail.com>
2017-09-12 12:36:59 -0500

    OAUTH

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '908792419025-2vbqaaj8355effvpojvtsci7naah9cev.apps.googleusercontent.com', "SD9r3cxRpFt1LRGRzbomb-z3"
+  provider :google_oauth2, ENV['GOOGLE_OAUTH_KEY'], ENV['GOOGLE_OAUTH_PASSWORD']
 end
